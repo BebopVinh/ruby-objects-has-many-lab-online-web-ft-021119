b66ed8f691297ac82b237fb5cf0264cda74ad5e0
Vinh Do <vinhdo07@gmail.com>
2019-02-25 21:23:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -13,10 +13,18 @@ class Post
   end
 
   def author_name
+<<<<<<< HEAD
     if self.author.class == NilClass
       nil
     else
       self.author.name
+=======
+    binding.pry
+    if self.author.name
+      self.author.name
+    else
+      nil
+>>>>>>> c3fe5cc4317adff314f2e4f776b48c7c7993bf4a
     end
   end
 end
